4048b229ae759efae6422447969139dcc22fba3e
Maurvick <82706197+Maurvick@users.noreply.github.com>
2024-10-09 17:31:46 +0300

    create localstoragehook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,23 +1,30 @@
 import { useEffect, useState } from 'react';
+import { Note } from '../models/Note';
 
 export function useLocalStorage<T>(
   key: string
-): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [value, setValue] = useState(() => {
+): [T | [], React.Dispatch<React.SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(() => {
     const jsonValue = localStorage.getItem(key);
-
-    // return use item if jsonValue not null
+    // return item if jsonValue not null
     if (jsonValue != null) {
       return JSON.parse(jsonValue);
     } else {
-      return '';
+      return [];
     }
   });
-
   // update localStorage if value changed
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
 
   return [value, setValue];
+}
+
+export function getNotes(): Note[] {
+  return JSON.parse(localStorage.getItem('notes') || '[]');
+}
+
+export function setNotes(notes: Note[]): void {
+  localStorage.setItem('notes', JSON.stringify(notes));
 }
